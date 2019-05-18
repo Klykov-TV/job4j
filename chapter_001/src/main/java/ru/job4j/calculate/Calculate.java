e8b9a3e809deeedb6ac099583c7273d2c0e91251
Klykov-TV <tim.klykov@gmail.com>
2019-05-18 16:49:13 +0300

    1.3. JavaDoc. Code convention[#142474]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
-
+/**
+ * Выводит на экран фразу Hello World
+ * @author Timofey Klykov 
+*/
 public class Calculate
 {
+	/**
+	* Main
+	* @param args - args
+	* @return Hello World
+	*/	
 	public static void main (String[] args)
 	{
 		System.out.println("Hello World");
