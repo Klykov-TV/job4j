11cac2fa94d0436efa2e001e756636273c68bf5b
Klykov-TV <tim.klykov@gmail.com>
2019-05-18 18:10:01 +0300

    1.3.1 Maven Plugins[#142470]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,8 +8,7 @@ public class Calculate
 	/**
 	* Main
 	* @param args - args
-	* @return Hello World
-	*/	
+	*/       
 	public static void main (String[] args)
 	{
 		System.out.println("Hello World");
