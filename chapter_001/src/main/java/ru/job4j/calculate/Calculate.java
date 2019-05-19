c5a44fc90f713324b3ff29517bc095d37974197f
Klykov-TV <tim.klykov@gmail.com>
2019-05-19 20:34:54 +0300

    1.5. Checkstyle[#142476]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,14 +3,12 @@ package ru.job4j.calculate;
  * Выводит на экран фразу Hello World
  * @author Timofey Klykov 
 */
-public class Calculate
-{
+public class Calculate {
 	/**
 	* Main
 	* @param args - args
 	*/       
-	public static void main (String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	
