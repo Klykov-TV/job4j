a4d68348668c3d8c3b8296ce5a7e37dc8587c4a8
Klykov-TV <tim.klykov@gmail.com>
2019-05-23 08:00:48 +0300

    3.3. Идеальный вес.[#142460]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,42 @@
 package ru.job4j.calculator;
 
+/**
+ * Калькулятор из 4 арифметических операций
+ */
 public class Calculator {
 
+    /**
+     * @param first первое значение
+     * @param second второе значение
+     * @return Сумму
+     */
     public double add(double first, double second) {
         return first + second;
     }
 
+    /**
+     * @param first первое значение
+     * @param second второе значение
+     * @return Разность
+     */
     public double subtract(double first, double second) {
         return first - second;
     }
 
+    /**
+     * @param first первое значение
+     * @param second второе значение
+     * @return Частное
+     */
     public double div(double first, double second) {
         return first / second;
     }
 
+    /**
+     * @param first первое значение
+     * @param second второе значение
+     * @return Произведение
+     */
     public double multiple(double first, double second) {
         return first * second;
     }
--- chapter_001/src/main/java/ru/job4j/conventer/Conventer.java
+++ chapter_001/src/main/java/ru/job4j/conventer/Conventer.java
@@ -13,14 +13,29 @@ public class Conventer {
         return (value / 70);
     }
 
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллары.
+     */
     public int rubleToUSD(int value) {
         return (value / 60);
     }
 
+    /**
+     * Конвертируем евро в рубли.
+     * @param value евро.
+     * @return Рубли.
+     */
     public int euroToRuble(int value) {
         return (value * 70);
     }
 
+    /**
+     * Конвертируем доллары в рубли.
+     * @param value доллары.
+     * @return рубли.
+     */
     public int usdToRuble(int value) {
         return (value * 60);
     }
