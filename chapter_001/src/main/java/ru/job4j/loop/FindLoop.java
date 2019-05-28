e9348ffa16bec27ebff42e40d3a4e38eac7e8421
Klykov-TV <tim.klykov@gmail.com>
2019-05-28 20:46:42 +0300

    6.1. Классический поиск перебором.[#142448]

---
+++ chapter_001/src/main/java/ru/job4j/loop/FindLoop.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+/**
+ * Класс ищет элемент в массиве
+ * @author Timofey Klykov
+ * @since 28.05.2019
+ */
+public class FindLoop {
+    /**
+     * @param data Массив, в котором ищем элемент
+     * @param el   Искомый элемент
+     * @return Индекс искомого элемента или -1 в случае отсутствия элемента
+     */
+    public int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import ru.job4j.loop.FindLoop;
+
+public class FindLoopTest {
+
+    FindLoop loop = new FindLoop();
+
+    @Test
+    public void whenEl3() {
+        int[] input = new int[]{1, 2, 3};
+        int value = 3;
+        int result = loop.indexOf(input, value);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenElMissing() {
+        int[] input = new int[]{4, 12, 83, 1};
+        int value = 8;
+        int result = loop.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
