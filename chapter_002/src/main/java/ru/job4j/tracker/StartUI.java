3f0e61fa739dadbfcde69dd77675246df28fb70b
Klykov-TV <tim.klykov@gmail.com>
2019-06-28 01:18:56 +0300

    1. Обеспечить бесперебойную работу приложения Tracker.[#142533]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 public class StartUI {
     private final Input input;
     private final Tracker tracker;
+    private int[] range = new int[] {0, 1, 2, 3, 4, 5};
 
     public StartUI(Input input, Tracker tracker) {
         this.input = input;
@@ -15,13 +16,11 @@ public class StartUI {
         do {
             System.out.println();
             menu.show();
-            int key = Integer.valueOf(input.ask("Select: "));
-            menu.select(key);
-        } while (!"y".equals(this.input.ask("Press Y to Exit:  ")));
+            menu.select(input.ask("Select: ", this.range));
+        } while (!"y".equals(this.input.ask("Press y to Exit:  ")));
     }
 
     public static void main(String[] args) {
-
-        new StartUI(new ConsoleInput(), new Tracker()).init();
+        new StartUI(new ValidateInput(), new Tracker()).init();
     }
 }
